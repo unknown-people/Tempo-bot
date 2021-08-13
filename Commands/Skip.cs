d2c83d0b82e88db2c0201a7c53155404bd389b49
BomboBombone <Marcusione34>
2021-08-13 23:31:58 +0200

    Add project files.

---
+++ Commands/Skip.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Gateway;
+using Discord.Commands;
+
+namespace Music_user_bot
+{
+    [Command("skip")]
+    public class SkipCommand : CommandBase
+    {
+        public override void Execute()
+        {
+            if (Program.CanModifyList(Client, Message))
+            {
+                var list = Program.TrackLists[Message.Guild.Id];
+
+                var currentSong = list.Tracks[0];
+                list.Tracks.RemoveAt(0);
+                currentSong.CancellationTokenSource.Cancel();
+
+                Message.Channel.SendMessage("Skipped the current song.");
+            }
+        }
+    }
+}
